b763104eee57c60ffe4dc5af4bb60ad1dc780580
yhdeon <deonprojdev@hotmail.com>
2025-11-14 17:19:12 +0800

    added some basic design for web app via bootstrap template

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+# app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static')
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/tables")
+def tables():
+    return render_template("tables.html")
+
+@app.route("/charts")
+def charts():
+    return render_template("charts.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
